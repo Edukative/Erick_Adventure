785e2c97070c2130025ce98d3265b323c83e482d
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-22 19:47:06 +0200

    Start the new project

---
+++ Erick adventure/Assets/Scripts/RubyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 position = transform.position;
+
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        transform.position = position;
+
+    }
+
+}
