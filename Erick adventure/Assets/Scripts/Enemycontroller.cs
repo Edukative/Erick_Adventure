1756cc99a225ae66168f71c03b5174c57526e57e
Edukative <56083723+Edukative@users.noreply.github.com>
2019-12-17 19:46:04 +0100

    finish the enemys

--- Erick adventure/Assets/Scripts/Enemycontroller.cs
+++ Erick adventure/Assets/Scripts/Enemycontroller.cs
@@ -4,22 +4,58 @@ using UnityEngine;
 
 public class Enemycontroller : MonoBehaviour
 {
-    public float speed;
+    public float speed; //speed of the enemy
     Rigidbody2D rb2D;
-    
+    public bool isVertical; // if it's not,it will walk horizontally
+
+    //timer values
+    float timer;
+    int direction = 1;
+    public float changeTime = 3.0f;
+
     // Start is called befor the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>(); // get the enemy's RigidBody
+        timer = changeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime; // make the timer go to 0
+        
+        if (timer < 0)
+        {
+            direction = -direction; // turns the direction into negative
+            timer = changeTime; // resets the timer
+        }
         Vector2 position = rb2D.position; // get the current position of the enemy
-        position.x = position.x + Time.deltaTime * speed; // sum the position x with the speed and the time
+        if (isVertical) // if the enemy walks vertically
+        {
+            // same as isVertical == true;
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+         // sum the position x with the speed and the time
 
         rb2D.MovePosition(position); // apply the previous sum position to the enemy's rigidbody
 
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>(); // get the script from the player
+
+        if (player != null) // if is not empty the variable
+        {
+            player.ChangeHealth(-1);
+        }
+
+
+    }
+
 }
