6ceff455090c58658122ca13ccca1e2789dcca74
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-14 19:48:30 +0100

    the proces of the animations

--- Erick adventure/Assets/Scripts/Enemycontroller.cs
+++ Erick adventure/Assets/Scripts/Enemycontroller.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemycontroller : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public float speed; //speed of the enemy
     Rigidbody2D rb2D;
@@ -13,11 +13,15 @@ public class Enemycontroller : MonoBehaviour
     int direction = 1;
     public float changeTime = 3.0f;
 
+    // animator values
+    Animator animator;
+
     // Start is called befor the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>(); // get the enemy's RigidBody
-        timer = changeTime;
+        timer = changeTime; // set the timer
+        animator = GetComponent<Animator>(); // get the same animator
     }
 
     // Update is called once per frame
@@ -25,7 +29,7 @@ public class Enemycontroller : MonoBehaviour
     {
         timer -= Time.deltaTime; // make the timer go to 0 accordind to the time.
         
-        if (timer < 0)
+        if (timer < 0) // reaches 0
         {
             direction = -direction; // turns the direction into negative
             timer = changeTime; // resets the timer
@@ -35,10 +39,16 @@ public class Enemycontroller : MonoBehaviour
         {
             // same as isVertical == true;
             position.y = position.y + Time.deltaTime * speed * direction;
+            // animator change values
+            animator.SetFloat("Move X", 0); // keep untouched since it isn't this axis
+            animator.SetFloat("Move Y", direction);
         }
         else // else condition,it wil go horizontally
         {
             position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0); // keep untouched since it isn't this axis
+
         }
          // sum the position x with the speed and the time
 
@@ -52,7 +62,7 @@ public class Enemycontroller : MonoBehaviour
 
         if (player != null) // if is not empty the variable
         {
-            player.ChangeHealth(-1);
+            player.ChangeHealth(-1); // damages the player
         }
 
 
