79b172d91afadf3ce3196b9489354bffd3113a8a
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-28 19:46:04 +0100

    The animations  of Ruby

--- Erick adventure/Assets/Scripts/RubyController.cs
+++ Erick adventure/Assets/Scripts/RubyController.cs
@@ -14,15 +14,19 @@ public class RubyController : MonoBehaviour
     float InvincibleTimer;
     public float timeInvisible = 2.0f;
     bool isInvencible;
-    
- 
- 
+
+    // animator values
+    Animator playerAnim;
+    Vector2 lookDirection = new Vector2(1, 0); // the direction is facing the player in the scene
+
     // Start is called before the first frame update
     void Start()
     {
         rubyRB2D = GetComponent<Rigidbody2D>(); // Get the player's Rigidbody
 
         currentHealth = maxHealth; // the current health is the max healt available to the player
+
+        playerAnim = GetComponent<Animator>(); // get the player's animator
      
     }
 
@@ -33,13 +37,27 @@ public class RubyController : MonoBehaviour
 
         float horizontal = Input.GetAxis("Horizontal"); // get the verticla input
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) // if it's moving
+
+        {
+            lookDirection.Set(move.x, move.y); // sets the vector already created
+
+            lookDirection.Normalize(); // just puts the number into 1 oe -1
+        }
+
+        // animator set values
+        playerAnim.SetFloat("Look X", lookDirection.x);
+        playerAnim.SetFloat("Look Y", lookDirection.y);
+        playerAnim.SetFloat("Speed", move.magnitude);
+
         Vector2 position = transform.position; // makes a vector based on current position
 
         position.x = position.x + speed * horizontal * Time.deltaTime;
         // the position is equal to the same position but a little bit bigger
 
         position.y = position.y + speed * vertical * Time.deltaTime; // called each second instead of each frame
-
 
         //transform.position = position; // saves this position to the current one
         rubyRB2D.MovePosition(position);
