98f90d20e88ea82d28ba9d47215286d83cdd5bd8
Edukative <56083723+Edukative@users.noreply.github.com>
2020-02-04 19:49:29 +0100

    put the waypoints

---
+++ Erick adventure/Assets/Scripts/DrawWaypoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWaypoint : MonoBehaviour
+{
+    public Transform target; // the next waypoint that the enemy goes
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draws a white line from this transform to the target
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position, target.position);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 0.25f);
+    }
+}
--- Erick adventure/Assets/Scripts/Enemycontroller.cs
+++ Erick adventure/Assets/Scripts/Enemycontroller.cs
@@ -16,12 +16,32 @@ public class EnemyController : MonoBehaviour
     // animator values
     Animator animator;
 
+    // waypoint values
+    public Vector2[] localNodes;
+    //private Vector2[] worldNodes;
+    int currentNode;
+    int nextNode;
+    Vector2 velocity;
+
     // Start is called befor the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>(); // get the enemy's RigidBody
         timer = changeTime; // set the timer
         animator = GetComponent<Animator>(); // get the same animator
+        // waypoint stuff
+        localNodes = new Vector2[transform.childCount];
+
+        for (int i = 0; i <= transform.childCount - 1; ++i)
+        {
+            // from 0, to the lenght of the list, while is not there, sum 1 to the counter
+            Transform child = transform.GetChild(i).transform;
+            // get the transform of the child in the loop
+            localNodes[i] = new Vector2(child.transform.position.x, child.transform.position.y);
+            // put in the index a new Vector that is the position of the child.
+            Debug.Log("index" + i + "Transform" + localNodes[i]);
+        }
+
     }
 
     // Update is called once per frame
